6bba4782cbe32367c9cf036f8784fb590b921027
Vella91 <velichka.suvandzhieva@mentormate.com>
2022-06-06 22:39:29 +0300

    WebDriver LOGIN click button test

--- src/test/java/selenium/FirstTest.java
+++ src/test/java/selenium/FirstTest.java
@@ -2,7 +2,6 @@ package selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -10,18 +9,18 @@ import java.util.List;
 
 public class FirstTest {
 
+    ChromeDriver driver;
+
     public static void main(String[] args) throws InterruptedException {
-
-        ChromeDriver driver = new ChromeDriver();
-
 
         //1st approach - setting chrome driver manually
 /*
-        System.setProperty("webdriver.chrome.drive", "drivers/chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver", "drivers/chromedriver.exe");
 */
-
         //2nd approach - use WebDriverManager library
         WebDriverManager.chromedriver().setup();
+        //initializing the driver
+        ChromeDriver driver = new ChromeDriver();
 
         //opening a Chrome window
         driver.get("http://training.skillo-bg.com/posts/all");
@@ -30,13 +29,15 @@ public class FirstTest {
         //creating a WebElement and then use the WebElement to click on it
         WebElement loginButton = driver.findElement(By.id("nav-link-login"));
         //list of elements
+/*
         List<WebElement> loginButtons = driver.findElements(By.id("nav-ling-login"));
+*/
         loginButton.click();
 
 
         //bad practice, use proper waits
         Thread.sleep(5000);
-        //alsways close the browser after test
+        //always close the browser after test
         driver.close();
     }
 }
