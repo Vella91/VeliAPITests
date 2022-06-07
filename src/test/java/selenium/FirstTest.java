8a2a9131d5626a04ba4d357da5a1e17bb9e27555
Vella91 <velichka.suvandzhieva@mentormate.com>
2022-06-07 10:47:19 +0300

    Main Class - LOGIN and New Post with Assert

--- src/test/java/selenium/FirstTest.java
+++ src/test/java/selenium/FirstTest.java
@@ -4,6 +4,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 
 import java.util.List;
 
@@ -34,9 +35,17 @@ public class FirstTest {
 */
         loginButton.click();
 
+        WebElement usernameField = driver.findElement(By.id("defaultLoginFormUsername"));
+        usernameField.sendKeys("test91");
+        WebElement passwordField = driver.findElement(By.id("defaultLoginFormPassword"));
+        passwordField.sendKeys("test91");
+        WebElement signInButton = driver.findElement(By.id("sign-in-button"));
+        signInButton.click();
+        WebElement newPostButton = driver.findElement(By.id("nav-link-new-post"));
+        Assert.assertTrue(newPostButton.isDisplayed());
 
         //bad practice, use proper waits
-        Thread.sleep(5000);
+      /*  Thread.sleep(5000);*/
         //always close the browser after test
         driver.close();
     }
