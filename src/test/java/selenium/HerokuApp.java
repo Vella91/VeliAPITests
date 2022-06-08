9ff739b297e6b52d711e82cac09cc1433c3715b2
Vella91 <velichka.suvandzhieva@mentormate.com>
2022-06-08 11:32:37 +0300

    Heroku App adding tests from Lecture

--- src/test/java/selenium/HerokuApp.java
+++ src/test/java/selenium/HerokuApp.java
@@ -1,23 +1,30 @@
 package selenium;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
+import java.time.Duration;
 import java.util.List;
 
 public class HerokuApp {
 
     WebDriver driver;
+    WebDriver wait;
+    Actions actions;
 
     @BeforeMethod
     public void setUp() {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        actions = new Actions(driver);
     }
 
     @AfterMethod
@@ -26,7 +33,7 @@ public class HerokuApp {
     }
 
     @Test
-    public void addRemoveElements() throws InterruptedException {
+    public void addRemoveElements1() throws InterruptedException {
         driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
     /*    WebElement deleteButton = driver.findElement(By.cssSelector(".deleteButton"));
         Assert.assertFalse(deleteButton.isDisplayed());*/
@@ -41,10 +48,54 @@ public class HerokuApp {
     }
 
     @Test
-    public void addRemoveElements2() {
+    public void addRemoveElements2() throws InterruptedException {
         driver.get("https://the-internet.herokuapp.com/add_remove_elements/");
         //test logic
-        WebElement addElementbutton = driver.findElement(By.xpath("//button[@onclick=\"addElement()\"]"));
+        List<WebElement> elementsContainerChildren = driver.findElements(By.xpath("//div[@id='elements']/descendant::*"));
+        WebElement addElementbutton = driver.findElement(By.xpath("//button[@onclick='addElement()']"));
+
+        for(int i=0;i<2;i++){
+            addElementbutton.click();
+        }
+        elementsContainerChildren = driver.findElements(By.xpath("//div[@id='elements']/descendant::*"));
+
+        Assert.assertEquals(elementsContainerChildren.size(), 2 );
+        Thread.sleep(1000);
     }
 
+    @Test
+    public void basicAuth() throws InterruptedException {
+     driver.get("https://admin:admin@the-internet.herokuapp.com/basic_auth");
+     WebElement text = driver.findElement(By.xpath("//div[@class='example']/p"));
+
+     Assert.assertEquals(text.getText(), "Congratulations! You must have the proper credentials.");
+
+     Thread.sleep(1000);
+    }
+
+    @Test
+    public void dragAndDrop(){
+        driver.get("https://the-internet.herokuapp.com/drag_and_drop");
+
+        WebElement elementA = driver.findElement(By.id("column-a"));
+        WebElement elementB = driver.findElement(By.id("column-b"));
+
+        actions.moveToElement(elementA).clickAndHold(elementA).moveToElement(elementB).release(elementB).build().perform();
+
+    }
+
+    @Test
+    public void contextMenu() throws InterruptedException {
+        driver.get("https://the-internet.herokuapp.com/context_menu");
+
+        WebElement contextBox = driver.findElement(By.id("hot-spot"));
+        actions.contextClick(contextBox).perform();
+        Alert alert = driver.switchTo().alert();
+
+        String alertText = alert.getText();
+        Assert.assertEquals(alertText, "You selected a context menu");
+        alert.dismiss();
+
+        Thread.sleep(1000);
+    }
 }
